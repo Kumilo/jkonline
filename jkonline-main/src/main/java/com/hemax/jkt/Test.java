9c06ffe66c69d2e88e3bb1adfc73e766ee75eb67
zhangquan_msxf <quan.zhang@msxf.com>
2016-06-12 17:22:29 +0800

    .

--- jkonline-main/src/main/java/com/hemax/jkt/Test.java
+++ jkonline-main/src/main/java/com/hemax/jkt/Test.java
@@ -1,9 +1,11 @@
-package com.dema.jkt;
+package com.hemax.jkt;
 
 public class Test {
 
+	
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		System.out.println(1);
 	}
 
