f7701e34470519a78dc63c089933277b31e79204
zhangquan_msxf <quan.zhang@msxf.com>
2016-06-27 20:40:53 +0800

    web正常跳转版本

--- jkonline-main/src/main/java/com/hemax/jkt/controller/MainController.java
+++ jkonline-main/src/main/java/com/hemax/jkt/controller/MainController.java
@@ -1,12 +1,11 @@
 package com.hemax.jkt.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import com.hemax.jkt.data.repository.MsgRepository;
-import com.hemax.jkt.domain.Msg;
 
 @RequestMapping("/cqjkt")
 @Controller
@@ -17,10 +16,10 @@ public class MainController {
 	MsgRepository msgRepository;
 	
 	@RequestMapping("/home")
-	public String home() {
+	public String home(Model model) {
 		
-		List<Msg> msg = msgRepository.findAll();
-		System.out.println(msg.size());
+		//List<Msg> msg = msgRepository.findAll();
+		model.addAttribute("message", "Black color");
 		return "hello";
 	}
 
